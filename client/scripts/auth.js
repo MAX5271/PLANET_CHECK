a7c4ecfb9d08cbf4ac8f1dd2c2472214052a6593
GyanPrakash2483 <gyanprakash2483@gmail.com>
2024-01-09 20:56:15 +0530

    Added basic authentication skeleton - unimplemented

---
+++ client/scripts/auth.js
@@ -0,0 +1,10 @@
+const auth_login = document.getElementById('auth_login')
+const auth_register = document.getElementById('auth_register');
+
+auth_login.onclick = function() {
+    
+}
+
+auth_register.onclick = function() {
+    
+}
